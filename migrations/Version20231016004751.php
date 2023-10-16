7c7fb26b6a5df473c0505821753467d1557430a8
kristaps gaidulis <kristaps.gaidulis@gmail.com>
2023-10-17 01:38:49 +0300

    fix migration

--- migrations/Version20231016004751.php
+++ migrations/Version20231016004751.php
@@ -21,15 +21,11 @@ final class Version20231016004751 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE exchange_rate (id INT AUTO_INCREMENT NOT NULL, base_currency VARCHAR(3) NOT NULL, target_currency VARCHAR(3) NOT NULL, exchange_rate DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE transaction ADD CONSTRAINT FK_723705D19B6B5FBA FOREIGN KEY (account_id) REFERENCES account (id)');
-        $this->addSql('CREATE INDEX IDX_723705D19B6B5FBA ON transaction (account_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE exchange_rate');
-        $this->addSql('ALTER TABLE transaction DROP FOREIGN KEY FK_723705D19B6B5FBA');
-        $this->addSql('DROP INDEX IDX_723705D19B6B5FBA ON transaction');
     }
 }
